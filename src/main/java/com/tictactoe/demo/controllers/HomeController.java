1ac4b1e8e80002aeccac73841a78fc7927dee405
gerard <geriloza@gmail.com>
2020-07-06 10:32:04 +0200

    falta putmapping

--- src/main/java/com/tictactoe/demo/controllers/HomeController.java
+++ src/main/java/com/tictactoe/demo/controllers/HomeController.java
@@ -5,18 +5,19 @@ import com.tictactoe.demo.entities.Player;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class HomeController {
-
+    static Board b =new Board();
+    static Player p1 = new Player('X',"Spartan");
+    static Player p2 = new Player('O',"Gladiator");
     @GetMapping("/")
     public String home(Model model){
-        Player p1 = new Player('X',"Spartan");
-        Player p2 = new Player('O',"Gladiator");
-        Board b = new Board();
         model.addAttribute("player1",p1);
         model.addAttribute("player2",p2);
         model.addAttribute("board",b);
         return "index";
     }
+
 }
--- src/main/java/com/tictactoe/demo/entities/Board.java
+++ src/main/java/com/tictactoe/demo/entities/Board.java
@@ -7,9 +7,20 @@ public class Board {
 
     public Board(){
         taulell=new char[size][size];
+        fillWithSymbol('X');
+    }
+    private void fillWithSymbol(char symbol){
+        for(int i=0;i<taulell.length;i++){
+            for(int j=0;j<taulell[i].length;j++){
+                taulell[i][j]=symbol;
+            }
+        }
     }
     public int getSize(){
         return size;
     }
 
+    public char[][] getTaulell() {
+        return taulell;
+    }
 }
